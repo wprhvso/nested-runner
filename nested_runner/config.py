242775779f1f0be5b57a04324f636ca953654bea
user <wprhvso@gmail.com>
2026-08-09 21:19:26 +0700

    chore: bump version

--- nested_runner/config.py
+++ nested_runner/config.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from nested_runner.errors import NestedError
 
 API_VERSION = "6.0-preview"
-USER_AGENT = "nested-runner/0.1.3"
+USER_AGENT = "nested-runner/0.2.0"
 
 POLL_TIMEOUT = 90
 REQUEST_TIMEOUT = 30
